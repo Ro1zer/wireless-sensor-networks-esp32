ff67958279f7b738b7abd7bfe682dbba767418c2
Maksym Oleksiienko <starsredmaks@gmail.com>
2025-10-28 16:35:13 +0200

    Small code refactor

--- src/receiver/main.cpp
+++ src/receiver/main.cpp
@@ -4,21 +4,33 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <ArduinoJson.h>
+#include <HardwareSerial.h>
+
+// Налаштування OLED-дисплея
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Налаштування WiFi для вебсервера
 const char *ssid = "Wokwi-GUEST";
 const char *password = "";
 WebServer server(80);
 String lastData = "{}";
+
+// Ініціалізація HC-12 через Serial2
+HardwareSerial HC12Serial(2);
+
 void handleRoot()
 {
   server.send(200, "application/json", lastData);
 }
+
 void setup()
 {
   Serial.begin(115200);
-  Serial2.begin(9600); // HC-12
+  HC12Serial.begin(9600, SERIAL_8N1, 17, 18); // HC-12 на GPIO17(RX2), GPIO18(TX2)
+
+  // Ініціалізація OLED-дисплея
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
     Serial.println("OLED error!");
@@ -26,6 +38,8 @@ void setup()
       ;
   }
   display.clearDisplay();
+
+  // Підключення до WiFi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
@@ -34,25 +48,33 @@ void setup()
     Serial.print(".");
   }
   Serial.println("OK");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Налаштування маршруту вебсервера
   server.on("/", handleRoot);
   server.begin();
 }
+
 void loop()
 {
   server.handleClient();
-  if (Serial2.available())
+
+  if (HC12Serial.available())
   {
-    String data = Serial2.readStringUntil('\n');
+    String data = HC12Serial.readStringUntil('\n');
     lastData = data;
+
     // Розбір JSON
     StaticJsonDocument<200> doc;
     DeserializationError err = deserializeJson(doc, data);
+
     if (!err)
     {
       float temp = doc["temp"];
       float hum = doc["hum"];
       int light = doc["light"];
+
       // Вивід у Serial Monitor
       Serial.print("Temp: ");
       Serial.print(temp);
@@ -60,7 +82,8 @@ void loop()
       Serial.print(hum);
       Serial.print("%, Light: ");
       Serial.println(light);
-      // Вивід на OLED
+
+      // Вивід на OLED-дисплей
       display.clearDisplay();
       display.setCursor(0, 0);
       display.setTextSize(1);
@@ -74,6 +97,10 @@ void loop()
       display.print("L: ");
       display.println(light);
       display.display();
+    }
+    else
+    {
+      Serial.println("JSON parse error!");
     }
   }
 }
--- src/transmitter/main.cpp
+++ src/transmitter/main.cpp
@@ -2,18 +2,33 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "DHT.h"
-#define DHTPIN 15
-#define DHTTYPE DHT22
+#include <HardwareSerial.h>
+
+// Визначення пінів для датчиків
+#define DHTPIN 16
 #define LDR_PIN 34
+#define DHTTYPE DHT22
+
+// Налаштування OLED-дисплея
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Ініціалізація датчика DHT
 DHT dht(DHTPIN, DHTTYPE);
+
+// Ініціалізація HC-12 через Serial2
+HardwareSerial HC12Serial(2);
+
 void setup()
 {
-  Serial.begin(115200); // Debug
-  Serial2.begin(9600);  // HC-12 на GPIO16 (RX2), GPIO17 (TX2)
+  Serial.begin(115200);                       // Для налагодження через USB
+  HC12Serial.begin(9600, SERIAL_8N1, 17, 18); // HC-12 на GPIO17(RX2), GPIO18(TX2)
+
+  // Ініціалізація DHT
   dht.begin();
+
+  // Ініціалізація OLED-дисплея
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
     Serial.println("OLED error!");
@@ -24,12 +39,15 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
 }
+
 void loop()
 {
+  // Зчитування даних з датчиків
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
   int light = analogRead(LDR_PIN);
-  // Вивід на OLED
+
+  // Вивід на OLED-дисплей
   display.clearDisplay();
   display.setCursor(0, 0);
   display.print("T: ");
@@ -41,14 +59,19 @@ void loop()
   display.print("L: ");
   display.println(light);
   display.display();
-  // Формуємо JSON
+
+  // Формуємо JSON-рядок
   String data = "{ \"temp\": " + String(temp, 1) +
                 ", \"hum\": " + String(hum, 1) +
-                ", \"light\": " + String(light) + " }\n";
+                ", \"light\": " + String(light) + " }";
+
   // Відправка через HC-12
-  Serial2.print(data);
+  HC12Serial.print(data);
+  HC12Serial.print('\n');
+
   // Для контролю також у Serial Monitor
   Serial.print("Sent: ");
   Serial.println(data);
+
   delay(2000);
 }
